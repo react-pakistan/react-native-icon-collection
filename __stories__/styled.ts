7d0484cb25b39e2001e7975cad905e9fff31f568
Taimoor Khan <taimoor.muhammad@arbisoft.com>
2020-04-12 14:07:01 +0500

    :recycle: upgraded dependencies

--- __stories__/styled.ts
+++ __stories__/styled.ts
@@ -4,16 +4,15 @@ export const StyledScrollView = styled.ScrollView``;
 
 export const StyledView = styled.View`
   flex: 1;
-  flex-direction: row;
-  flex-wrap: wrap;
+  flex-flow: row wrap;
 `;
 
 export const IconWrapper = styled.View`
+  align-items: center;
+  flex-direction: column;
   height: 100px;
   margin: 20px;
   width: 100px;
-  flex-direction: column;
-  align-items: center;
 `;
 
 export const TextWrapper = styled.Text``;
