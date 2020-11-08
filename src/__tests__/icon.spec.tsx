c4cfc09dbd5366e8b57b2fabeab8204f18607bcb
Taimoor Khan <taimoor.muhammad@arbisoft.com>
2020-11-08 08:53:00 +0500

    :recycle: test changes

--- src/__tests__/icon.spec.tsx
+++ src/__tests__/icon.spec.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { Icon } from '../icon';
 
-describe('Testing Icon component', () => {
-  it('matches Icon snapshot,', () => {
+describe('Testing Icon component', () : void => {
+  it('matches Icon snapshot,', () : void => {
     const wrapper = renderer.create(
       <Icon icon="BlockchainColor1" />,
     );
