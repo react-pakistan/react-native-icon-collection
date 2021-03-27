bd1bd3bf5853bc8a43690c1120652529357f2eb3
Taimoor Khan <taimoor.muhammad@arbisoft.com>
2021-03-27 08:22:37 +0500

    :recycle: upgraded dependencies

--- __stories__/index.tsx
+++ __stories__/index.tsx
@@ -1,9 +1,8 @@
-/* eslint-disable global-require, import/no-default-export */
-
+import AsyncStorage from '@react-native-community/async-storage';
 import { theme } from '@react-pakistan/util-react-native-functions';
 import { getStorybookUI, configure } from '@storybook/react-native';
 import React from 'react';
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components/native';
 import { loadStories } from './story-loader';
 
 import './rn-addons';
@@ -13,7 +12,12 @@ configure(() => {
   loadStories();
 }, module);
 
-const Storybook = getStorybookUI({ port: 9002, host: 'localhost', onDeviceUI: true });
+const Storybook = getStorybookUI({
+  asyncStorage: AsyncStorage || null,
+  host: 'localhost',
+  onDeviceUI: true,
+  port: 9002,
+});
 
 // Refer to https://github.com/storybookjs/storybook/tree/master/app/react-native#start-command-parameters
 // To find allowed options for getStorybookUI
